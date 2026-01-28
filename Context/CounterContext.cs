da219ce6304ef049b961b0e513badd6e897a1cc0
OKadam <OKadam@flexngate.com>
2026-01-28 14:32:51 -0500

    added counter context

---
+++ Context/CounterContext.cs
@@ -0,0 +1,16 @@
+﻿using counterAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace counterAPI.Context
+{
+    public class CounterContext:DbContext
+    {
+        public CounterContext(DbContextOptions<CounterContext> options)
+            : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Counter> Counters { get; set; } = null!;
+    }
+}
